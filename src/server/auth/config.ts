4aac5a5cf1ede8f412033db99db2abdbd9db07f1
Aditya Mishra <mishraaaditya29@gmail.com>
2025-04-23 11:40:51 +0530

    imported login schema in auth config for validation

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -2,6 +2,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import DiscordProvider from "next-auth/providers/discord";
+import { loginSchema } from "~/schemas/auth";
 
 import { db } from "~/server/db";
 
