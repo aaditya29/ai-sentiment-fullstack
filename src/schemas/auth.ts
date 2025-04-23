e5ae4163169a56919c86b2736d976cd3c23af186
Aditya Mishra <mishraaaditya29@gmail.com>
2025-04-23 10:31:12 +0530

    added signup schema validation to auth.ts

--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -6,3 +6,11 @@ export const loginSchema = z.object({
 });
 
 export type LoginSchema = z.infer<typeof loginSchema>;
+
+export const signupSchema = z
+  .object ({
+    name:z.string().min(3, "Name must be at least 3 characters long"),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(8, "Password must be at least 8 characters long"),
+    confirmPassword: z.string(),
+  })
